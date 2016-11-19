b8befd3c3e333ae997065bcfe2d61722273d6e6e
whatifif <whatifif@yahoo.com>
2016-11-19 17:31:34 +1100

    AWS is added to widgetBaseUrl

--- src/containers/Weather/config.js
+++ src/containers/Weather/config.js
@@ -1,5 +1,5 @@
 const config = {
-  widgetBaseUrl: 'http://localhost:3000/weather-widget'
+  widgetBaseUrl: __DEVELOPMENT__ ? 'http://localhost:3000/weather-widget' : 'http://13.54.49.146/weather-widget'
 };
 
 export default config;
